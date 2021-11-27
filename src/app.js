a3e48c891e90f5f461e89c9bf6b976638d770bc6
LavRed <lavisha.redmon1@gmail.com>
2021-11-27 12:25:11 -0500

    Added days to forecast and loop js

--- src/app.js
+++ src/app.js
@@ -21,6 +21,33 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="36"
+              />
+              <div class="weather-forecast-temp">
+                <span class="weather-forecast-temp-max">18° </span>
+                <span class="weather-forecast-temp-min"> 12° </span>
+              </div>
+            </div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let iconElement = document.querySelector("#icon");
   let timeElement = document.querySelector("#time");
@@ -85,3 +112,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
